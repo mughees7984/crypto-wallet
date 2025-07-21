cb7a88a9e5a769459496d399f21f408b6f7b6d38
Muhammad Mughees <mughees791@gmail.com>
2025-07-21 17:47:13 +0500

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { NetworkProvider } from "./Context/NetworkContext.jsx";
@@ -9,14 +10,16 @@ import { BalanceProvider } from "./Context/BalanceContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <NetworkProvider>
-      <WalletProvider>
-        <TransactionProvider>
-         <BalanceProvider>
-           <App />
-         </BalanceProvider>
-        </TransactionProvider>
-      </WalletProvider>
-    </NetworkProvider>
+    <HashRouter>
+      <NetworkProvider>
+        <WalletProvider>
+          <TransactionProvider>
+            <BalanceProvider>
+              <App />
+            </BalanceProvider>
+          </TransactionProvider>
+        </WalletProvider>
+      </NetworkProvider>
+    </HashRouter>
   </StrictMode>
 );
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,8 +4,9 @@ import { Routes, Route } from "react-router-dom";
 
 import WalletSetup from "../components/WalletSetup";
 import CreateWallet from "../components/CreateWallet";
-import ImportWallet from "../pages/ImportWallet";
+import ImportWallet from "../components/ImportWallet";
 import WalletUI from "../pages/WalletUI";
+import ConfirmSend from "../components/Send/ConfirmSend";
 
 export default function AppRoutes() {
   return (
@@ -14,6 +15,7 @@ export default function AppRoutes() {
       <Route path="/create-wallet" element={<CreateWallet />} />
       <Route path="/import" element={<ImportWallet />} />
       <Route path="/wallet" element={<WalletUI />} />
+      <Route path="/confirm-send" element={<ConfirmSend />} />
     </Routes>
   );
 }
